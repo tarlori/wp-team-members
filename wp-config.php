128942c6b018e7d1972b8ae68d6e8e9d946c4d4f
Lorand Tar <tarlori@yahoo.com>
2016-10-19 17:15:35 +0300

    reset config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cf_test');
+define('DB_NAME', 'xxxx');
 
 /** MySQL database username */
-define('DB_USER', 'cf_test');
+define('DB_USER', 'xxxx');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'eSKh8aS57vc6vgLJ');
+define('DB_PASSWORD', 'xxxx');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
